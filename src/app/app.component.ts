aaf5943cb2939856419aa2420e3865eb9e417a1c
king-martinien <martinienfokoue@yahoo.com>
2024-11-29 03:07:22 +0100

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,18 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, inject, OnInit, WritableSignal} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {HttpClient} from '@angular/common/http';
+import {JsonPipe} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
   title = 'dgi-portal';
+  data!: any;
   private readonly http: HttpClient = inject(HttpClient);
 
   ngOnInit() {
@@ -20,6 +22,7 @@ export class AppComponent implements OnInit {
       {
         next: (data) => {
           console.log('data : ', data);
+          this.data = data;
         },
         error: (err) => {
           console.log("ERROR : ", err);
